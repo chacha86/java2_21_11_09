ce8d7f7262ea43b5e52ca91f1a9f1a2d62b990db
chacha86 <taejincha0619@gmail.com>
2021-11-30 19:53:10 +0900

    게시물 정렬2 -> 내림차순 정렬 및 정렬 대상 적용

--- board/Board.java
+++ board/Board.java
@@ -71,7 +71,7 @@ public class Board {
 		System.out.println("정렬 방법을 선택해주세요. (1. 오름차순,  2. 내림차순) :");
 		int type = Integer.parseInt(sc.nextLine());
 		
-		Collections.sort(boardArticles, new ArticleComparator());
+		Collections.sort(boardArticles, new ArticleComparator(type, target));
 		list(boardArticles);
 	}
 
@@ -447,15 +447,42 @@ public class Board {
 }
 
 class ArticleComparator implements Comparator<BoardArticle> {
-
+	int type;// 1. 오름차순, 2. 내림차순
+	int target; // 1. 번호, 2. 조회수
+	
+	ArticleComparator(int type, int target) {
+		this.type = type;
+		this.target = target;
+	}
+	
 	@Override
 	public int compare(BoardArticle o1, BoardArticle o2) {
 		
-		if(o1.hit > o2.hit) {
-			return 1;
+		int result = getCompareResult(o1, o2);
+		
+		if(type == 2) {
+			result *= -1;
 		}
 		
-		return -1;
+		return result;
+		
+	}
+
+	private int getCompareResult(BoardArticle o1, BoardArticle o2) {
+		
+		if(target == 1) { // 번호
+			if(o1.id > o2.id) {
+				return 1;
+			}
+			
+			return -1;
+		} else { // 조회수
+			if(o1.hit > o2.hit) {
+				return 1;
+			}
+			
+			return -1;
+		}
 	}
 	
 }
